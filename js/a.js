e2b4c674ad038074a64b326691b4d160ebb507a0
linyingzhen <277715645@qq.com>
2017-06-26 16:44:03 +0800

    20170626164354

---
+++ js/a.js
@@ -0,0 +1,5 @@
+var a='a';
+function showa (argument) {
+	console.log('a');
+}
+showa();
---
+++ js/b.js
@@ -0,0 +1,6 @@
+var b='b';
+function showb (argument) {
+	console.log('b');
+}
+console.log('bbb');
+showb();
